d0ecf6df85aab272dd96fec4cb5a1fcbe4d10905
@f.torquato <fabricio.torquato1@gmail.com>
2017-10-01 22:14:49 -0300

    add Despojados

---
+++ MARATONA DE PROGRAMAÇÃO SBC 2017/Despojados.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long n;
+    cin>>n;
+    unsigned long long f = 2;
+    int factors = 0;
+    while (f*f <= n)
+    {
+        if (n % f == 0)
+        {
+            factors++;
+            while (n % f == 0) n /= f;
+        }
+        f++;
+    }
+    if (n != 1) factors++;
+    cout<<((unsigned long long)pow(2,factors)) - (factors + 1)<<endl;
+    return 0;
+}
